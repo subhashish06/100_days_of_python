f0636b0baa60fcf05392876c33e69ef3134d7dfb
Subhashish Dhar <subhashish06@gmail.com>
2021-06-08 20:17:40 +0530

    style changes

--- Day-14/higher_lower_game.py
+++ Day-14/higher_lower_game.py
@@ -18,30 +18,30 @@ print(f"Compare B: {contestant_2['name']}, a {contestant_2['description']}, from
 user_choice = input("Who has more followers on Instagram? Type 'A' or 'B': ").upper()
 
 
-def check_answer(contestant_1, contestant_2, user_choice):
+def check_answer(contestant_a, contestant_b, choice):
     """
     input : the two contestants and the user answer
     output : return True is user_choice is correct otherwise False
     """
-    if contestant_1['follower_count'] > contestant_2['follower_count']:
+    if contestant_a['follower_count'] > contestant_b['follower_count']:
         answer = 'A'
     else:
         answer = 'B'
-    if user_choice == answer:
+    if choice == answer:
         return True
     else:
         return False
 
 
-def get_contestants_for_next_round(contestant_2):
+def get_contestants_for_next_round(contestant):
     """
     returns a tuple of two contestants for the next round
     """
-    contestant_1 = contestant_2
-    contestant_2 = random.choice(data)
-    while contestant_2 == contestant_1:
-        contestant_2 = random.choice(data)
-    return (contestant_1, contestant_2)
+    contestant_a = contestant
+    contestant_b = random.choice(data)
+    while contestant_b == contestant_a:
+        contestant_b = random.choice(data)
+    return contestant_a, contestant_b
 
 
 while check_answer(contestant_1, contestant_2, user_choice):
