ef9804df7ef6b0b3cf4701d34adc72201c4ab1ad
Subhashish Dhar <subhashish06@gmail.com>
2021-06-12 13:45:34 +0530

    quiz game

---
+++ Day-17/question_model.py
@@ -0,0 +1,5 @@
+class Question:
+    """takes a text and an answer : creates a question object."""
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
---
+++ Day-17/quiz_brain.py
@@ -0,0 +1,23 @@
+class QuizBrain:
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.question_list = question_list
+        self.score = 0
+
+    def still_has_question(self):
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q{self.question_number}: {question.text}. (True/False): ").lower()
+        self.check_answer(user_answer, question.answer)
+
+    def check_answer(self, user_ans, correct_ans):
+        if user_ans.lower() == correct_ans.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's wrong!")
+        print(f"The correct answer was: {correct_ans}")
+        print(f"Your current score is {self.score}/{self.question_number}.\n")
