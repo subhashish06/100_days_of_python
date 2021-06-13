f574cea986130f2aa5812f1123cb58c6d553cf3f
Subhashish Dhar <subhashish06@gmail.com>
2021-06-13 08:46:08 +0530

    turtle

---
+++ Day-18/square_turtle.py
@@ -0,0 +1,15 @@
+# Look for colors : https://cs111.wellesley.edu/labs/lab01/colors
+
+from turtle import Turtle, Screen
+
+my_screen = Screen()
+my_screen.bgcolor('cornsilk2')
+
+timmy = Turtle()
+timmy.color('cyan3')
+
+for _ in range(4):
+    timmy.forward(100)
+    timmy.left(90)
+
+my_screen.exitonclick()
