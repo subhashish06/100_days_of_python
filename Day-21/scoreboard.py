348567c96c31ba4bfaf9a17fcaa02e739bdf3693
Subhashish Dhar <subhashish06@gmail.com>
2021-06-17 09:24:31 +0530

    snake game high score added

--- Day-21/scoreboard.py
+++ Day-21/scoreboard.py
@@ -11,11 +11,11 @@ class ScoreBoard(Turtle):
         self.color('white')
         self.hideturtle()
         self.penup()
-        self.goto(0, 270)
+        self.goto(-150, 270)
 
     def display_score(self):
         self.clear()
-        self.write(f'Your score : {self.score}', align=ALIGNMENT, font=FONT)
+        self.write(f'Your Score : {self.score}', align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
@@ -23,3 +23,32 @@ class ScoreBoard(Turtle):
     def display_game_over(self):
         self.home()
         self.write(f'GAME OVER', align=ALIGNMENT, font=FONT)
+
+
+class HighScore(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color('white')
+        self.hideturtle()
+        self.penup()
+        self.goto(150, 270)
+        self.display_high_score()
+
+    def display_high_score(self):
+        with open('high_score_record.txt', 'r') as f:
+            high_score = f.read().rstrip('\n')
+            self.clear()
+            self.write(f'High Score : {high_score}', align=ALIGNMENT, font=FONT)
+
+    @staticmethod
+    def update_high_score(score):
+        with open('high_score_record.txt', 'r') as f:
+            high_score = f.read().rstrip('\n')
+        if score > int(high_score):
+            with open('high_score_record.txt', 'w') as f:
+                f.write(str(score))
+
+    @staticmethod
+    def reset_high_score():
+        with open('high_score_record.txt', 'w') as f:
+            f.write('0')
--- Day-21/snake_game_main.py
+++ Day-21/snake_game_main.py
@@ -1,7 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
-from scoreboard import ScoreBoard
+from scoreboard import ScoreBoard, HighScore
 import time
 
 DELAY = 0.2
@@ -10,7 +10,7 @@ DELAY = 0.2
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
-screen.title('Snake Game')
+screen.title("Snake Game [Press 'R' key to reset the high score]")
 screen.tracer(0)
 
 # Create the snake
@@ -21,6 +21,7 @@ food = Food()
 
 # Create the score board
 score_board = ScoreBoard()
+high_score = HighScore()
 
 # Define the keystrokes to move the snake
 screen.listen()
@@ -28,6 +29,7 @@ screen.onkeypress(fun=snake.left, key='Left')
 screen.onkeypress(fun=snake.right, key='Right')
 screen.onkeypress(fun=snake.down, key='Down')
 screen.onkeypress(fun=snake.up, key='Up')
+screen.onkeypress(fun=high_score.reset_high_score, key='r')
 
 # Move the snake
 is_game_over = False
@@ -45,11 +47,13 @@ while not is_game_over:
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         score_board.display_game_over()
         is_game_over = True
+        high_score.update_high_score(score=score_board.score)
     # Detect collision with itself
     for segment in snake.snake_segments[1:]:
         if snake.head.distance(segment) < 10:
             score_board.display_game_over()
             is_game_over = True
+            high_score.update_high_score(score = score_board.score)
 
 
 screen.exitonclick()
