2e73739309aad6e74b67d81b50b2dbf0dbf498e8
Subhashish Dhar <subhashish06@gmail.com>
2021-07-04 07:49:32 +0530

    password generator

--- Day-29-password_generator_GUI/main.py
+++ Day-29-password_generator_GUI/main.py
@@ -1,7 +1,39 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
+def generate():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    letters_in_password = random.sample(letters, random.randint(8, 10))
+    symbols_in_password = random.sample(symbols, random.randint(2, 4))
+    numbers_in_password = random.sample(numbers, random.randint(2, 4))
+
+    final_list = letters_in_password + symbols_in_password + numbers_in_password
+
+    random.shuffle(final_list)
+    password = ''.join(final_list)
+
+    if website_entry.get() != '' and username_entry.get() != 0:
+        password_entry.delete(0, END)
+        password_entry.insert(0, password)
+    else:
+        messagebox.showinfo(title='Data Missing', message="Please provide website and username "
+                                                          "before generating password")
+
+# ---------------------------- COPY PASSWORD ------------------------------- #
+
+
+def copy():
+    if password_entry.get() != '':
+        pyperclip.copy(password_entry.get())
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -34,13 +66,13 @@ canvas.grid(row=0, column=1)
 website_label = Label(text='Website:')
 website_label.grid(row=1, column=0)
 website_entry = Entry(width=50)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1, columnspan=3)
 website_entry.focus()
 
 username_label = Label(text='Email/Username:')
 username_label.grid(row=2, column=0)
 username_entry = Entry(width=50)
-username_entry.grid(row=2, column=1, columnspan=2)
+username_entry.grid(row=2, column=1, columnspan=3)
 username_entry.insert(0, 'sdhar@example.com')
 
 password_label = Label(text='Password:')
@@ -48,10 +80,13 @@ password_label.grid(row=3, column=0)
 password_entry = Entry(width=32)
 password_entry.grid(row=3, column=1)
 
-generate_button = Button(text='Generate Password')
-generate_button.grid(row=3, column=2)
+generate_button = Button(text='Generate', command=generate)
+generate_button.grid(row=3, column=3)
+
+copy_button = Button(text='Copy', command=copy)
+copy_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=42, command=save)
-add_button.grid(row=4, column=1, columnspan=2)
+add_button.grid(row=4, column=1, columnspan=3)
 
 window.mainloop()
