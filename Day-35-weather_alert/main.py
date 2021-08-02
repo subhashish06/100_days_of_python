3d3975854d9431851b238cb777f8e36da1fc524d
Subhashish Dhar <subhashish06@gmail.com>
2021-08-02 08:18:19 +0530

    adding environ variables

--- Day-35-weather_alert/main.py
+++ Day-35-weather_alert/main.py
@@ -1,39 +1,43 @@
 from twilio.rest import Client
 import requests
+import os
 
-API_KEY = "64671c189476dcfdbb3309e18f10c7b8"
-LATITUDE = 24.819799
-LONGITUDE = 79.145599
+API_KEY = os.environ.get("OWM_API_KEY")
+LATITUDE = 12.971599
+LONGITUDE = 77.594566
 
 parameters = {
     "lat": LATITUDE,
     "lon": LONGITUDE,
     "appid": API_KEY,
-    "exclude": "current,minutely,daily",
+    "exclude": "hourly,minutely,daily",
     "units": "metric"
 }
 
 account_sid = 'AC563da7de98225d587e5aea9ab11b275d'
-auth_token = 'fd07b208690287722533d3d1c2852ef7'
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 
 response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
 response.raise_for_status()
 weather_data = response.json()
 
-will_rain = False
-for i in range(12):
-    rain_code = weather_data["hourly"][i]["weather"][0]["id"]
-    print(rain_code)
-    if int(rain_code) < 700:
-        will_rain = True
-        break
+temperature = weather_data["current"]["temp"]
+weather = weather_data["current"]["weather"][0]["description"]
 
-if will_rain:
-    client = Client(account_sid, auth_token)
-    message = client.messages.create(
-            from_='whatsapp:+14155238886',
-            body='It might rain. Bring an Umbrella!',
-            to='whatsapp:+919739983563'
-            )
+client = Client(account_sid, auth_token)
+# WhatsAPP
+message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=f'Currently in Bangalore:\n\nTemperature : {temperature}\nWeather : {weather}',
+        to='whatsapp:+919739983563'
+        )
 
-    print(message.status)
+print(message.sid)
+# SMS
+message = client.messages.create(
+    messaging_service_sid='MG3db36b400f9d11a13e0c50d9c110e45f',
+    body=f'Currently in Bangalore:\n\nTemperature : {temperature}\nWeather : {weather}',
+    to='+919739983563'
+    )
+
+print(message.sid)
