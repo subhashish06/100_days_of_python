282bcdcd0fd63a03fc56aefb21b15a7131923b70
Subhashish Dhar <subhashish06@gmail.com>
2021-08-06 19:46:02 +0530

    export password

--- Day-32-emails and dates/send_motivational_quotes.py
+++ Day-32-emails and dates/send_motivational_quotes.py
@@ -1,22 +1,24 @@
 from smtplib import SMTP
 from datetime import datetime
 from random import choice
+import os
 
 date = datetime.now()
 
-if date.weekday() == 0:
-    with open("quotes.txt") as f:
-        quotes = f.readlines()
-        quote_of_the_day = choice(quotes)
-        print(quote_of_the_day)
+with open("quotes.txt") as f:
+    quotes = f.readlines()
+    quote_of_the_day = choice(quotes)
+    print(quote_of_the_day)
 
-    with SMTP("smtp.gmail.com") as mail:
-        mail.starttls()
-        mail.login(user="subhashish.py@gmail.com", password="Python@123")
-        mail.sendmail(
-                    from_addr="subhashish.py@gmail.com",
-                    to_addrs=["subhashish06@gmail.com", "bananivista2017@gmail.com"],
-                    msg=f"Subject:Monday Motivation\n\n"
-                        f"Hi,\n\nQuote of the Day:\n{quote_of_the_day}\n\nStay Motivated!\n\n"
-                        f"Regards,\nSubhashish"
-                    )
+PASSWORD = os.environ.get("GMAIL_PASSWORD")
+
+with SMTP("smtp.gmail.com") as mail:
+    mail.starttls()
+    mail.login(user="subbu.pybot@gmail.com", password=PASSWORD)
+    mail.sendmail(
+                from_addr="subbu.pybot@gmail.com",
+                to_addrs=["subhashish06@gmail.com"],
+                msg=f"Subject:Monday Motivation\n\n"
+                    f"Hi,\n\nQuote of the Day:\n{quote_of_the_day}\n\nStay Motivated!\n\n"
+                    f"Regards,\nSubhashish"
+                )
