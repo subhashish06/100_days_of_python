cdcd6607a7f00c90abe055bedbd6c4afea36134a
Subhashish Dhar <subhashish06@gmail.com>
2022-05-25 15:55:16 +0530

    handle deprecated methods

--- Day-48-selenium/selenium_intro.py
+++ Day-48-selenium/selenium_intro.py
@@ -5,6 +5,7 @@ Date: 20/09/2021
 """
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 CHROME_DRIVER_PATH = "C:\\Development\\chromedriver"
 WEB_URL = "https://www.python.org/"
@@ -12,15 +13,13 @@ WEB_URL = "https://www.python.org/"
 driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
 driver.get(WEB_URL)
 
-dates = driver.find_elements_by_xpath(
-    '//*[@id="content"]/div/section/div[2]/div[2]/div/ul/li/time'
-)
+dates = driver.find_elements(by=By.XPATH, value='//*[@id="content"]/div/section/div[3]/div[2]/div/ul/li/time')
 dates = [date.text for date in dates]
+print(dates)
 
-events = driver.find_elements_by_xpath(
-    '//*[@id="content"]/div/section/div[2]/div[2]/div/ul/li/a'
-)
+events = driver.find_elements(by=By.XPATH, value='//*[@id="content"]/div/section/div[3]/div[2]/div/ul/li/a')
 events = [event.text for event in events]
+print(events)
 
 result = {}
 for index, time in enumerate(dates):
